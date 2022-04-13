2a80227e42df362d4dbd1f05bd62dd8a31604c8a
Breen-C <103140498+Breen-C@users.noreply.github.com>
2022-04-13 11:05:09 -0700

    Update dataVisualization.r

--- dataVisualization.r
+++ dataVisualization.r
@@ -12,3 +12,15 @@ ggplot(data=dogs)+geom_point(aes(x=height,y=weight))
 ggplot(data=dogs,aes(x=datadog,y=popularity))+geom_text(aes(label=breed))+geom_point()
        
 ggplot(data=dogs,aes(x=datadog,y=popularity,color=group))+geom_text(aes(label=breed))+geom_point()
+
+
+
+
+# to get data for the first try at our data 
+ball = read.table("2019volleyball.csv", check.names = F, header = TRUE, sep = ",", row.names = 
+1)
+
+View(ball)
+str(ball)
+
+ggplot(data=ball,aes(x= Spike ,y= Height))+geom_point()
